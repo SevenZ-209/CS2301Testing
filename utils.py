338933c87c9a9c888344f60b73af19ef59e8269f
Khoa <lekhoa5263@gmail.com>
2026-03-06 14:20:08 +0700

    doi sang tieng viet

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import math
 
 def is_prime(n):
     if n < 2:
-        raise ValueError('Invalid input')
+        raise ValueError('Đầu vào không hợp lệ')
     for i in range(2, math.isqrt(n)):
         if n % i == 0:
             return False

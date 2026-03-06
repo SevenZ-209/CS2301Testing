bd4d952ff8bd6f8d39800e6a418c89476a35d334
Khoa <lekhoa5263@gmail.com>
2026-03-06 14:21:17 +0700

    return false

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import math
 
 def is_prime(n):
     if n < 2:
-        raise ValueError('Invalid input')
+        return False
     for i in range(2, math.isqrt(n)):
         if n % i == 0:
             return False
